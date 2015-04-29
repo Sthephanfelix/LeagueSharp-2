b61d4d3c8aff957433b58dfbe2e07a5e72d2c904
Sthephanfelix <elkorean@dkdk.clsco>
2015-04-30 01:13:49 +0900

    Update AssemblyInfo.cs

--- Third Party/Xerath/Properties/AssemblyInfo.cs
+++ Third Party/Xerath/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xerath")]
+[assembly: AssemblyProduct("hitchance edited xerath")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
