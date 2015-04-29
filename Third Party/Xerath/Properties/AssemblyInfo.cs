e82d1cd963d493bf580e8bc25523a328e9a7d177
Sthephanfelix <elkorean@dkdk.clsco>
2015-04-30 01:21:39 +0900

    Update AssemblyInfo.cs

--- Third Party/Xerath/Properties/AssemblyInfo.cs
+++ Third Party/Xerath/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("hitchance edited xerath")]
+[assembly: AssemblyTitle("Xerath")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("hitchance edited xerath")]
+[assembly: AssemblyProduct("Xerath")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
